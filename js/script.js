30b386ce0870fcbc411e249f03b51792402a5bdf
Michael <mishanhzer@gmail.com>
2023-08-22 11:01:52 +0500

    add cycle for and conditions if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,12 +10,22 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?');
-const b = prompt('На сколько оцените его?');
-const c = prompt('Один из последних просмотренных фильмов?');
-const d = prompt('На сколько оцените его?');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?');
+    const b = +prompt('На сколько оцените его?');
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        console.log('Good!');
+        personalMovieDB['movies'][a] = b;
+    } else {
+        console.log('Error!');
+        i--;
+    }
+}
+if (personalMovieDB['count'] < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} if (personalMovieDB['count'] > 10 && personalMovieDB['count'] < 30) {
+    console.log('Вы классичесий зритель');
+} if (personalMovieDB['count'] > 30) {
+    console.log('Вы киноман');
+}
 console.log(personalMovieDB);
-
