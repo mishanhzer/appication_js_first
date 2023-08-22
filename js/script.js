8a1bb2f6629b3a7a3c587ecc35751d03582caadf
Michael <mishanhzer@gmail.com>
2023-08-22 13:00:01 +0500

    add example cicle while

--- js/script.js
+++ js/script.js
@@ -10,22 +10,35 @@ const personalMovieDB = {
     privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?');
+//     const b = +prompt('На сколько оцените его?');
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         console.log('Good!');
+//         personalMovieDB['movies'][a] = b;
+//     } else {
+//         console.log('Error!');
+//         i--;
+//     }
+// }
+
+let i = 0;
+while (i < 2) {
+    i++;
     const a = prompt('Один из последних просмотренных фильмов?');
     const b = +prompt('На сколько оцените его?');
     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-        console.log('Good!');
         personalMovieDB['movies'][a] = b;
     } else {
-        console.log('Error!');
+        console.log('Error');
         i--;
     }
 }
-if (personalMovieDB['count'] < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} if (personalMovieDB['count'] > 10 && personalMovieDB['count'] < 30) {
-    console.log('Вы классичесий зритель');
-} if (personalMovieDB['count'] > 30) {
-    console.log('Вы киноман');
-}
+// if (personalMovieDB['count'] < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } if (personalMovieDB['count'] > 10 && personalMovieDB['count'] < 30) {
+//     console.log('Вы классичесий зритель');
+// } if (personalMovieDB['count'] > 30) {
+//     console.log('Вы киноман');
+// }
 console.log(personalMovieDB);
